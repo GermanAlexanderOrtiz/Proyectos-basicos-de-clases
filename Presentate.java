6ffc854286e71c359ffd3cb589360592548b84c8
German Ortiz <157213891+GermanAlexanderOrtiz@users.noreply.github.com>
2025-08-01 22:21:56 -0500

    Add files via upload
    Proyecto sobre una autopresentacion, algo simple con solo unos cuantos println

---
+++ Presentate.java
@@ -0,0 +1,6 @@
+public class Presentate {
+
+    public static void main(String[] args) {
+        System.out.println("**************************\n\nNombre: German Alexander Ortiz Peña\nEdad: 19 años\nPais: Colombia\n\n**************************");
+    }
+}
