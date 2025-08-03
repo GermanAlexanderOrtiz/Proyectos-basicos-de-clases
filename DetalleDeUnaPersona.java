16dda374909ccc57f52ce38c50b13a7d78a93bb6
German Ortiz <157213891+GermanAlexanderOrtiz@users.noreply.github.com>
2025-08-03 07:18:11 -0500

    Add files via upload

---
+++ DetalleDeUnaPersona.java
@@ -0,0 +1,18 @@
+public class DetalleDeUnaPersona {
+    public static void main(String[] args) {
+     
+        String nombre = "German Alexander Ortiz Peña";
+        int edad = 19;
+        float altura = 1.68F;
+        String paisOrigen = "Venezuela";
+        char estadoCivil = 'C'; // C para casado S para soltero
+        // imprimimos los valores
+    
+        System.out.println(nombre);
+        System.out.println(edad);
+        System.out.println(altura);
+        System.out.println(paisOrigen);
+        System.out.println(estadoCivil);
+    }
+
+}
