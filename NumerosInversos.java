997877c5d6c54c13977f62d7f75c09b340d3aad7
German Ortiz <157213891+GermanAlexanderOrtiz@users.noreply.github.com>
2025-09-07 21:47:44 -0500

    Add files via upload

---
+++ NumerosInversos.java
@@ -0,0 +1,11 @@
+public class NumerosInversos {
+    public static void main(String[] args) {
+        System.out.println("*** Numeros Inversos ***");
+
+        var contador = 10;
+
+        while (contador >=1) {
+            System.out.println(contador--);
+        }
+    }
+}
