c6ce8e74fd17784c67a100b5fb922da87a23342b
German Ortiz <157213891+GermanAlexanderOrtiz@users.noreply.github.com>
2025-09-07 21:47:11 -0500

    Add files via upload

---
+++ NumeroParesFor.java
@@ -0,0 +1,10 @@
+public class NumeroParesFor {
+    public static void main(String[] args) {
+        System.out.println("*** Números Pares Con Ciclo For ***");
+
+        for (int i = 0; i <= 20; i++) {
+            if(i %2 == 0)
+            System.out.print(i + " ");
+        }
+    }
+}
